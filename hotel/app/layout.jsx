33b80620911d0ab4656b5e93458b51b3c4928f56
Ibrohim <ibrokhim.tjk@icloud.com>
2025-10-22 22:48:42 +0500

    ...

--- hotel/app/layout.jsx
+++ hotel/app/layout.jsx
@@ -23,6 +23,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        style={{ backgroundColor: "#ffffff", color: "#000000" }}
       </body>
     </html>
   );
